c101c722f906624cf769b8533264651bb3863d84
crypticcfrost <liamclyde.2001@gmail.com>
2025-03-26 14:01:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import os
 print("This is an example python program")
 time.sleep(2)
 print("We are going to print numbers from 0-10 every 3 seconds to demonstrate runtime . . .")
-time.sleep(2)
+time.sleep(3)
 os.system('cls' if os.name == 'nt' else 'clear')
 for i in range(1, 11):
     time.sleep(1)
     print(i)
 print("Program ending, clearing terminal now . . .")
+time.sleep(2)
 os.system('cls' if os.name == 'nt' else 'clear')
