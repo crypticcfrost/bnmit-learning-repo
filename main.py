89de3e678215f09f94c4ac14e4afee6ae482d55e
crypticcfrost <liamclyde.2001@gmail.com>
2025-03-26 13:57:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import time
+import os
 print("This is an example python program")
 time.sleep(3)
-
-print("We are going to print numbers from 0-10 every 3 seconds to demonstrate runtime")
+print("We are going to print numbers from 0-10 every 3 seconds to demonstrate runtime . . .")
+os.system('cls' if os.name == 'nt' else 'clear')
 for i in range(1, 11):
-    print(i)
     time.sleep(3)
+    print(i)
 print("End of program")
