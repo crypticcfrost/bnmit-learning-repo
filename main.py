e18ffd4851a407ddfcf15a1c9bd8b71d0f07ac11
crypticcfrost <liamclyde.2001@gmail.com>
2025-03-26 14:05:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ time.sleep(2)
 os.system('cls' if os.name == 'nt' else 'clear')
 
 print("We can also make the terminal seem fast moving and cool by shortening the delay . . .")
+time.sleep(4)
+print("\n\n\n\n")
+print("Ready?")
+time.sleep(2)
 arr = ["Permission denied? Challenge accepted.", "Decrypting payload...", "404: Ethics Not Found", "sudo rm -rf /world", "Brute forcing into system framework.."]
 for elements in arr:
-    time.sleep(0.2)
+    time.sleep(0.5)
     print(elements)
 
 print("Looks cool doesn't it? Now we're gonna clear the terminal again.")
