39ba2f539268ef4293fd7c6e255750ae607dc890
crypticcfrost <liamclyde.2001@gmail.com>
2025-03-26 13:55:56 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import time
 print("This is an example python program")
+time.sleep(3)
+
+print("We are going to print numbers from 0-10 every 3 seconds to demonstrate runtime")
+for i in range(1, 11):
+    print(i)
+    time.sleep(3)
+print("End of program")
